2abbdf1382fd4a65cf5a00b0868a7c61c93f3760
晚安 <a1033078928@gmail.com>
2022-06-28 16:22:53 +0800

    图片相关微调，由于图库访问慢，还是存在问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
   themeConfig: {
     nav: require("./nav"),
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: 'img/gif/panda.jpg', // 导航栏logo
+    logo: 'https://i.postimg.cc/2yr3430s/panda.jpg', // 导航栏logo  img/gif/panda.jpg
     repo: 'user-h/hdata-doc', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
